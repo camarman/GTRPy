db70dc779d6e4fbab51704e2a7dadfeffb48c53f
Arman Çam <arman-cam@windowslive.com>
2021-06-18 21:02:21 +0300

    Update GRTC_GUI.py
    Changing the color theme of the GUI and making some changes in the comments

--- GRTC Main/GRTC_GUI.py
+++ GRTC Main/GRTC_GUI.py
@@ -3,19 +3,16 @@
 import PySimpleGUI as sg
 from PIL import Image
 from sympy import *
-from GUI_eqn_producer import tensor_producer, tensor_component_producer
+from GUI_eqn_producer import *
 
 
 # Color option of the GUI
-sg.ChangeLookAndFeel('BlueMono')
-
+sg.ChangeLookAndFeel('Material1')
 
 # Defining some of the most common symbols in physics
 G, M, m, c, a, k, r_s = symbols('G, M, m, c, a, k, r_s')
-alpha, beta, gamma, delta, eta, epsilon, kappa = symbols(
-    'alpha, beta, gamma, delta, eta, epsilon, kappa')
-mu, nu, sigma, tau, chi, psi, omega = symbols(
-    'mu, nu, sigma, tau, chi, psi, omega')
+alpha, beta, gamma, delta, eta, epsilon, kappa = symbols('alpha, beta, gamma, delta, eta, epsilon, kappa')
+mu, nu, sigma, tau, chi, psi, omega = symbols('mu, nu, sigma, tau, chi, psi, omega')
 
 
 # Defining the used tensors in the GRTC
@@ -33,7 +30,7 @@ tensor_type3 = ['ddd', 'udd', 'uud', 'uuu']
 tensor_type4 = ['dddd', 'uddd', 'uudd', 'uuud', 'uuuu']
 
 
-# Defining the tensors that will be used with the tensor types
+# Classifying some of the tensors that will be used with the tensor types
 tensor2d = ['Metric Tensor', 'Ricci Tensor',
             'Traceless Ricci Tensor', 'Einstein Tensor']
 scalars = ['Ricci Scalar', 'Kretschmann Scalar']
@@ -42,23 +39,26 @@ scalars = ['Ricci Scalar', 'Kretschmann Scalar']
 # Defining the most common coordinate system symbols
 coordinates = ['t', 'x', 'y', 'z', 'r', 'theta', 'phi', 'rho']
 
-def re_size(xtensor, xtensor_type = ''):
+
+def re_size(xtensor_name, xtensor_type = ''):
     """
     Re-sizing the image of a tensor
 
     Args:
-        xtensor [ndim-array]: The given tensor 
+        xtensor_name [str]: The name of the desired tensor given as a string
+        xtensor_type [str]: Type of the tensor. Given in terms of 'u': contravariant
+        and 'd': covariant
     """
     im = Image.open('tensor.png')
-    if xtensor == 'Metric Tensor' and xtensor_type == 'ud': # for some reason this tensor looks bigger
-         size = (300, 300)
-    elif xtensor in tensor2d or xtensor == 'Inverse Metric Tensor':
+    if xtensor_name == 'Metric Tensor' and xtensor_type == 'ud': # for some reason this tensor looks bigger
+        size = (300, 300)
+    elif xtensor_name in tensor2d or xtensor_name == 'Inverse Metric Tensor':
         size = (500, 500)
-    elif xtensor == 'Christoffel Symbol':
+    elif xtensor_name == 'Christoffel Symbol':
         size = (1200, 600)
-    elif xtensor == 'Riemann Tensor' or xtensor == 'Weyl Tensor':
+    elif xtensor_name == 'Riemann Tensor' or xtensor_name == 'Weyl Tensor':
         size = (600, 600)
-    elif xtensor in scalars:
+    elif xtensor_name in scalars:
         size = (500, 500)
     im.thumbnail(size, Image.ANTIALIAS)
     out_dim = im.size
@@ -69,7 +69,7 @@ def re_size(xtensor, xtensor_type = ''):
 
 def re_size_component():
     """
-    Re-sizing the image of the tensor component
+    Re-sizing the image of the tensor components
     """
     im = Image.open('tensor_component.png')
     size = (200, 200)
@@ -85,9 +85,9 @@ def gui_main_process(diag_comp, coord_sys, desired_tensor):
     The main process of the GUI that processes the images for given metric, and coordinate system
 
     Args:
-        coord_sys [symbol]: The chosen coordinate system (cartesian, spherical, etc.)
         diag_comp [list]: Diagonal components of the metric tensor
-        desired_tensor [string]: The tensor that is chose by the user
+        coord_sys [list]: The coordinate system (cartesian, spherical, etc.)
+        desired_tensor [str]: Tensor chosen by the user
     """
     tensor_equation = tensor_producer(diag_comp, coord_sys, desired_tensor)
     tensor_component_equation = tensor_component_producer(diag_comp, coord_sys, desired_tensor)
@@ -313,9 +313,3 @@ if event == 'Submit':
                 desired_tensor = values[12] # getting the desired tensor type
                 coord_sys = symbols(values[1] + ' ' + values[3] + ' ' + values[5]) # getting the coordinate system
                 gui_main_process(diag_comp, coord_sys, desired_tensor)
-    
-
-
-            
-            
-        
