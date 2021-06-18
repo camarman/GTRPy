c51ade929d8e097e455a4a8d3241babc75477d1c
Arman Çam <arman-cam@windowslive.com>
2021-06-18 21:03:41 +0300

    Update GUI_eqn_producer.py
    Changing some of the comments and variable names

--- GRTC Main/GUI_eqn_producer.py
+++ GRTC Main/GUI_eqn_producer.py
@@ -4,114 +4,113 @@ from GRTC import *
 from sympy import latex
 
 
-def tensor_producer(diag_comp, coord_sys, xtensor, xtensor_type=''):
+def tensor_producer(diag_comp, coord_sys, xtensor_name, xtensor_type=''):
     """
     The function that will obtain inputs from the GUI in the GRTC_GUI.py file to produce
     tensor equation in the latex form
 
     Args:
-        coord_sys [symbol]: The chosen coordinate system (cartesian, spherical, etc.)
         diag_comp [list]: Diagonal components of the metric tensor
-        xtensor [string]: The name of the desired tensor given as a string
-        xtensor_type [string]: The type of the tensor. Given in terms of 'u': contravariant
+        coord_sys [list]: The coordinate system (cartesian, spherical, etc.)
+        xtensor_name [str]: The name of the desired tensor given as a string
+        xtensor_type [str]: The type of the tensor. Given in terms of 'u': contravariant
         and 'd': covariant
 
     Returns:
         Tensor equation in the latex form
     """
-    
-    if xtensor == 'Metric Tensor':
+    if xtensor_name == 'Metric Tensor':
         mt = MetricTensor(diag_comp, coord_sys)
         metric_tensor = mt.get_metrictensor()
-        if xtensor_type == '':   # the default type of the given tensor
+        if xtensor_type == '':   # default type of the given tensor
             return '$$' + latex(metric_tensor) + '$$'
         else: # changing the type of the tensor and hence the equation as well
             return '$$' + latex(mt.vary_metrictensor_type(metric_tensor, xtensor_type)) + '$$'
     
-    if xtensor == 'Inverse Metric Tensor':
+    if xtensor_name == 'Inverse Metric Tensor':
         mt = MetricTensor(diag_comp, coord_sys)
         inverse_metric_tensor = mt.get_inverse()
         return '$$' + latex(inverse_metric_tensor) + '$$'
     
-    if xtensor == 'Christoffel Symbol':
+    if xtensor_name == 'Christoffel Symbol':
         cs = ChristoffelSymbol(diag_comp, coord_sys)
         chris_symbol = cs.get_christoffelsymbol()
-        if xtensor_type == '':
+        if xtensor_type == '':   # default type of the given tensor
             return '$$' + latex(chris_symbol) + '$$'
         else:
             return '$$' + latex(cs.vary_christoffelsymbol_type(chris_symbol, xtensor_type)) + '$$'
     
-    if xtensor == 'Riemann Tensor':
+    if xtensor_name == 'Riemann Tensor':
         rt = RiemannTensor(diag_comp, coord_sys)
         riemann_tensor = rt.get_riemanntensor()
-        if xtensor_type == '':
+        if xtensor_type == '':   # default type of the given tensor
             return '$$' + latex(riemann_tensor) + '$$'
         else:
             return '$$' + latex(rt.vary_riemanntensor_type(riemann_tensor, xtensor_type)) + '$$'
     
-    if xtensor == 'Ricci Tensor':
+    if xtensor_name == 'Ricci Tensor':
         rit = RicciTensor(diag_comp, coord_sys)
         ricci_tensor = rit.get_riccitensor()
-        if xtensor_type == '':
+        if xtensor_type == '':   # default type of the given tensor
              return '$$' + latex(ricci_tensor) + '$$'
         else:
             return '$$' + latex(rit.vary_riccitensor_type(ricci_tensor, xtensor_type)) + '$$'
     
-    if xtensor == 'Ricci Scalar':
+    if xtensor_name == 'Ricci Scalar':
         rs = RicciScalar(diag_comp, coord_sys)
         ricci_scalar = rs.get_ricciscalar()
         return '$$R = ' + latex(ricci_scalar) + '$$'
     
-    if xtensor == 'Traceless Ricci Tensor':
+    if xtensor_name == 'Traceless Ricci Tensor':
         trt = TracelessRicciTensor(diag_comp, coord_sys)
         traceless_ricci_tensor = trt.get_trclss_riccitensor()
-        if xtensor_type == '':
+        if xtensor_type == '':   # default type of the given tensor
             return '$$' + latex(traceless_ricci_tensor) + '$$'
         else:
             return '$$' + latex(trt.vary_trclss_riccitensor_type(traceless_ricci_tensor, xtensor_type)) + '$$'
     
-    if xtensor == 'Weyl Tensor':
+    if xtensor_name == 'Weyl Tensor':
         wyl = WeylTensor(diag_comp, coord_sys)
         weyl_tensor = wyl.get_weyltensor()
-        if xtensor_type == '':
+        if xtensor_type == '':   # default type of the given tensor
             return '$$' + latex(weyl_tensor) + '$$'
         else:
             return '$$' + latex(wyl.vary_weyltensor_type(weyl_tensor, xtensor_type)) + '$$'
     
-    if xtensor == 'Einstein Tensor':
+    if xtensor_name == 'Einstein Tensor':
         et = EinsteinTensor(diag_comp, coord_sys)
         einstein_tensor = et.get_einsteintensor()      
-        if xtensor_type == '':
+        if xtensor_type == '':   # default type of the given tensor
             return '$$' + latex(einstein_tensor) + '$$'
         else:
-            return '$$' + latex(et.vary_einsteintensor_type(einstein_tensor,  xtensor_type)) + '$$'
+            return '$$' + latex(et.vary_einsteintensor_type(einstein_tensor, xtensor_type)) + '$$'
     
-    if xtensor == 'Kretschmann Scalar':
+    if xtensor_name == 'Kretschmann Scalar':
         ks = KretschmannScalar(diag_comp, coord_sys)
         kret_scalar = ks.get_kretschmannscalar()
         return '$$K = ' + latex(kret_scalar) + '$$'
                  
 
-def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtensor_type=''):
+def tensor_component_producer(diag_comp, coord_sys, xtensor_name, component='', xtensor_type=''):
     """
     The function that will obtain inputs from the GUI in the GRTC_GUI.py file to produce
     equations of the tensor components in the latex form
 
     Args:
-        coord_sys [symbol]: The chosen coordinate system (cartesian, spherical, etc.)
         diag_comp [list]: Diagonal components of the metric tensor
-        xtensor [string]: The name of the desired tensor given as a string
-        component [string]: The desired component of the tensor
-        xtensor_type [string]: The type of the tensor. Given in terms of 'u': contravariant
+        coord_sys [list]: The coordinate system (cartesian, spherical, etc.)
+        xtensor_name [str]: The name of the desired tensor given as a string
+        component [str]: The desired component of the tensor
+        xtensor_type [str]: Type of the tensor. Given in terms of 'u': contravariant
         and 'd': covariant
 
     Returns:
         Tensor component in the latex form
     """
-    if xtensor == 'Metric Tensor':
+    if xtensor_name == 'Metric Tensor':
         mt = MetricTensor(diag_comp, coord_sys)
         metric_tensor = mt.get_metrictensor()
-        if component == '': # the default case 
+        if component == '':   # default case 
             return '$$g_{{{0} {1}}} = {2}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(metric_tensor[0][0]))
         else:
             new_metric_tensor = mt.vary_metrictensor_type(metric_tensor, xtensor_type)
@@ -124,20 +123,20 @@ def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtens
             else:
                 return '$$g^{{{0} {1}}} = {2}$$'.format(latex(component[0]), latex(component[1]), latex(new_metric_tensor[i,j]))
     
-    if xtensor == 'Inverse Metric Tensor':
+    if xtensor_name == 'Inverse Metric Tensor':
         mt = MetricTensor(diag_comp, coord_sys)
         inverse_metric_tensor = mt.get_inverse()
-        if component == '':
+        if component == '':   # default case 
             return '$$g^{{{0} {1}}} = {2}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(inverse_metric_tensor[0][0]))
         else:
             i = coord_sys.index(component[0])
             j = coord_sys.index(component[1])
             return '$$g^{{{0} {1}}} = {2}$$'.format(latex(component[0]), latex(component[1]), latex(inverse_metric_tensor[i,j]))
 
-    if xtensor == 'Christoffel Symbol':
+    if xtensor_name == 'Christoffel Symbol':
         cs = ChristoffelSymbol(diag_comp, coord_sys)
         chris_symbol = cs.get_christoffelsymbol()
-        if component == '':
+        if component == '':   # default case 
             return '$$\\Gamma^{{{0}}}{{}}_{{{1} {2}}} = {3}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(coord_sys[0]), latex(chris_symbol[0][0][0]))
         else:
             new_chris_symbol = cs.vary_christoffelsymbol_type(chris_symbol, xtensor_type)
@@ -153,10 +152,10 @@ def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtens
             else:
                 return '$$\\Gamma^{{{0} {1} {2}}} = {3}$$'.format(latex(component[0]), latex(component[1]), latex(component[2]), latex(new_chris_symbol[i,j,k]))
     
-    if xtensor == 'Riemann Tensor':
+    if xtensor_name == 'Riemann Tensor':
         rt = RiemannTensor(diag_comp, coord_sys)
         riemann_tensor = rt.get_riemanntensor()
-        if component == '':
+        if component == '':   # default case 
              return '$$R^{{{0}}}{{}}_{{{1} {2} {3}}} = {4}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(coord_sys[0]), latex(coord_sys[0]), latex(riemann_tensor[0][0][0][0]))
         else:
             new_riemann_tensor = rt.vary_riemanntensor_type(riemann_tensor, xtensor_type)
@@ -175,10 +174,10 @@ def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtens
             else:
                 return '$$R^{{{0} {1} {2} {3}}}= {4}$$'.format(latex(component[0]), latex(component[1]), latex(component[2]), latex(component[3]), latex(new_riemann_tensor[i,j,k,l]))
                 
-    if xtensor == 'Ricci Tensor':
+    if xtensor_name == 'Ricci Tensor':
         rit = RicciTensor(diag_comp, coord_sys)
         ricci_tensor = rit.get_riccitensor()
-        if component == '':
+        if component == '':   # default case 
             return '$$R_{{{0} {1}}} = {2}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(ricci_tensor[0][0]))
         else:
             new_ricci_tensor = rit.vary_riccitensor_type(ricci_tensor, xtensor_type)
@@ -191,10 +190,10 @@ def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtens
             else:
                 return '$$R^{{{0} {1}}} = {2}$$'.format(latex(component[0]), latex(component[1]), latex(new_ricci_tensor[i,j]))
     
-    if xtensor == 'Traceless Ricci Tensor':
+    if xtensor_name == 'Traceless Ricci Tensor':
         trt = TracelessRicciTensor(diag_comp, coord_sys)
         traceless_ricci_tensor = trt.get_trclss_riccitensor()
-        if component == '':
+        if component == '':   # default case
             return '$$Z_{{{0} {1}}} = {2}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(traceless_ricci_tensor[0][0]))
         else:
             new_traceless_ricci_tensor = trt.vary_trclss_riccitensor_type(traceless_ricci_tensor, xtensor_type)
@@ -208,10 +207,10 @@ def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtens
                 return '$$Z^{{{0} {1}}} = {2}$$'.format(latex(component[0]), latex(component[1]), latex(new_traceless_ricci_tensor[i,j]))
                 
     
-    if xtensor == 'Einstein Tensor':
+    if xtensor_name == 'Einstein Tensor':
         et = EinsteinTensor(diag_comp, coord_sys)
         einstein_tensor = et.get_einsteintensor()
-        if component == '':
+        if component == '':   # default case
             return '$$G_{{{0} {1}}} = {2}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(einstein_tensor[0][0]))
         else:
             new_einstein_tensor = et.vary_einsteintensor_type(einstein_tensor, xtensor_type)
@@ -225,10 +224,10 @@ def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtens
                 return '$$G^{{{0} {1}}} = {2}$$'.format(latex(component[0]), latex(component[1]), latex(new_einstein_tensor[i,j]))
             
         
-    if xtensor == 'Weyl Tensor':
+    if xtensor_name == 'Weyl Tensor':
         wyl = WeylTensor(diag_comp, coord_sys)
         weyl_tensor = wyl.get_weyltensor()
-        if component == '':
+        if component == '':   # default case
             return '$$C_{{{0} {1} {2} {3}}} = {4}$$'.format(latex(coord_sys[0]), latex(coord_sys[0]), latex(coord_sys[0]), latex(coord_sys[0]), latex(weyl_tensor[0][0][0][0]))
         else:
             new_weyl_tensor = wyl.vary_weyltensor_type(weyl_tensor, xtensor_type)
@@ -247,15 +246,3 @@ def tensor_component_producer(diag_comp, coord_sys, xtensor, component='', xtens
             else:
                 return '$$C^{{{0} {1} {2} {3}}}= {4}$$'.format(latex(component[0]), latex(component[1]), latex(component[2]), latex(component[3]), latex(new_weyl_tensor[i,j,k,l]))
             
-            
-        
-        
-           
-                    
-        
-            
-        
-                
-            
-        
-        
